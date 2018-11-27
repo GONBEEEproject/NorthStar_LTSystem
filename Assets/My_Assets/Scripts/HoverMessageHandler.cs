37efbe4f4b109e2243382cf402263068e287d036
GONBEEE_project_MPlexPC <gonbeee.project@gmail.com>
2018-11-27 16:29:40 +0900

    Hover選択時に発火されるメソッドだけ実装

---
+++ Assets/My_Assets/Scripts/HoverMessageHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverMessageHandler : MonoBehaviour {
+
+    #region SlideControll
+
+    public void LoadSlide()
+    {
+
+    }
+
+    public void Activate()
+    {
+
+    }
+
+    public void Inactivate()
+    {
+
+    }
+
+    public void Next()
+    {
+
+    }
+
+    public void Previous()
+    {
+
+    }
+
+    #endregion
+
+    #region DroneControll
+
+
+
+
+    #endregion
+}
